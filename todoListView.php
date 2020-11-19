4d669ba5680426a158662bc07fa6dc62a1cdcc4d
LAPTOP-9CFOKKTI\SiBaChen <s107213036@mail1.ncnu.edu.tw>
2020-11-19 15:46:20 +0800

    改改改

--- todoListView.php
+++ todoListView.php
@@ -3,7 +3,7 @@ session_start();
 if (! isset($_SESSION['uID']) or $_SESSION['uID']<="") {
 	header("Location: loginForm.php");
 } 
-if ($_SESSION['uID']=='principal'){
+if ($_SESSION['uID']=='jc'){
 	$bossMode = 1;
 } 
 if ($_SESSION['uID']=='secretary'){
@@ -11,6 +11,9 @@ if ($_SESSION['uID']=='secretary'){
 }
 if ($_SESSION['uID']=='teacher'){
 	$bossMode = 3;
+}
+if ($_SESSION['uID']=='principal'){
+	$bossMode = 4;
 }
 else {
 	$bossMode=0;
@@ -38,8 +41,8 @@ $result=getJobList($bossMode);
 <p>my Todo List !! </p>
 <hr />
 <div><?php echo $msg; ?></div><hr>
-<a href="loginForm.php">login</a> | <a href="todoEditForm.php?id=-1">Add Task</a> <br>
-<table width="700" border="1">
+<a href="loginForm.php">login</a> | <?php if ($bossMode==0)  echo "<a href='申請Form.php'> 申請 </a>" ?> <br>
+<table width="1000" border="1">
   <tr>
     <td>編號</td>
     <td>姓名</td>
@@ -47,7 +50,12 @@ $result=getJobList($bossMode);
 	<td>爸爸</td>
     <td>媽媽</td>
 	<td>申請種類</td>
-	<td>-</td>
+	<td>老師蓋章</td>
+    <td>校長蓋章</td>
+    <td>秘書蓋章</td>
+    <td>老師備註</td>
+    <td>金額</td>
+    <td>秘書備註</td>
   </tr>
 <?php
 
@@ -59,7 +67,36 @@ while (	$rs=mysqli_fetch_assoc($result)) {
     echo "<td>" , htmlspecialchars($rs['Mom']), "</td>";
     echo "<td>" , htmlspecialchars($rs['FundType']), "</td>";
     if ($bossMode==0){
-        echo "<td>" , htmlspecialchars($rs['Tsing']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['TSign']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['PSign']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['SSign']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['TExplain']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['Fund']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['SExplain']), "</td>";
+    }
+    if ($bossMode==4){
+        echo "<td>" , htmlspecialchars($rs['TSign']), "</td>";
+        echo "<td>","<a href='todoSetControl.php?act=psign&id={$rs['applied']}'>OK</a>  ","<td>";
+        echo "<td>" , htmlspecialchars($rs['SSign']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['TExplain']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['Fund']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['SExplain']), "</td>";
+    }
+    if ($bossMode==2){
+        echo "<td>" , htmlspecialchars($rs['TSign']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['PSign']), "</td>";
+        echo "<td>","<a href='todoSetControl.php?act=ssign&id={$rs['applied']}'>簽名</a>  ","<td>";
+        echo "<td>" , htmlspecialchars($rs['TExplain']), "</td>";
+        echo "<td>" , "補助", "</td>";
+        echo "<td>" , "意見", "</td>";
+    }
+    if ($bossMode==3){
+        echo "<td>" , "<a href='todoSetControl.php?act=tsign&id={$rs['applied']}'>簽名</a>  ", "</td>";
+        echo "<td>" , htmlspecialchars($rs['PSign']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['SSign']), "</td>";
+        echo "<td>" , "老師意見", "</td>";
+        echo "<td>" , htmlspecialchars($rs['Fund']), "</td>";
+        echo "<td>" , htmlspecialchars($rs['SExplain']), "</td>";
     }
 	echo "</td></tr>";
 }
