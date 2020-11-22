dc2e199556492507d037bed7b12e398993679f5e
DESKTOP-I4A0C56\fanny <s107213039@mail1.ncnu.edu.tw>
2020-11-22 12:46:23 +0800

    可以加新資料了

--- apply.php
+++ apply.php
@@ -1,17 +1,19 @@
 <?php
-require("todoModel.php");
+require("dbconnect.php");
 $StdName=mysqli_real_escape_string($conn,$_POST['StdName']);
 $StdID=mysqli_real_escape_string($conn,$_POST['StdID']);
 $Dad=mysqli_real_escape_string($conn,$_POST['Dad']);
 $Mom=mysqli_real_escape_string($conn,$_POST['Mom']);
 $FundType=mysqli_real_escape_string($conn,$_POST['FundType']);
 
-if ($StdName) { 
-	addJob($StdName, $StdID, $Dad, $Mom, $FundType);
-	$msg="Message updateded";
+if ($StdName) { //if title is not empty
+	$sql = "insert into exam (applied,StdName, StdID, Dad, Mom, FundType,TSign, PSign, SSign, TExplain, Fund, SExplain, status) values (NULL,'$StdName', '$StdID', '$Dad', '$Mom', '$FundType',0,0,0,'TExplain',0,'SExplain',0);";
+	mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
+	echo "申請已新增完成! ";
 } else {
-	$msg= "Message title cannot be empty";
+	echo "請填入申請人姓名!";
 }
-header("Location: todoListView.php?m=$msg");
 ?>
-u
+
+</hr>
+<a href='todoListView.php'>back<a>
